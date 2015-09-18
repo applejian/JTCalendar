d0b95003753df1ee0f1306dc4bff5a8d183a668e
Nick O'Neill <nick.oneill@gmail.com>
2015-09-18 11:11:40 -0700

    Added an auxilliary view.

--- JTCalendar/Views/JTCalendarDayView.h
+++ JTCalendar/Views/JTCalendarDayView.h
@@ -15,6 +15,7 @@
 
 @property (nonatomic) NSDate *date;
 
+@property (nonatomic) UIView *assignmentView;
 @property (nonatomic, readonly) UIView *circleView;
 @property (nonatomic, readonly) UIView *dotView;
 @property (nonatomic, readonly) UILabel *textLabel;
